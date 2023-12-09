b1f629b80526214784cc8acf196ab1a48c2a5106
Vlad Foroshcha <vladfo2003@gmail.com>
2023-12-09 14:14:57 +0200

    feat: install needed libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FeedbackModule } from './feedback/feedback.module';
+import { FeedbackModule } from './modules/feedback/feedback.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionsFilter } from './utils/filters/all-exceptions.filter';
+import { LoggingInterceptor } from './utils/interceptors/logging.interceptor';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './utils/services/winston/winston';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FeedbackModule],
+  imports: [
+    FeedbackModule,
+    WinstonModule.forRoot(winstonConfig()),
+    ConfigModule.forRoot({
+      envFilePath: `env/.${process.env.NODE_ENV}.env`,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
