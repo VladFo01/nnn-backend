0b518c757b29c717c6a8996dd9af034d6f12a297
Vlad Foroshcha <vladfo2003@gmail.com>
2023-12-10 19:35:52 +0200

    feat: resolve deps

--- src/modules/feedback/feedback.module.ts
+++ src/modules/feedback/feedback.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { FeedbackService } from './feedback.service';
 import { FeedbackController } from './feedback.controller';
 import { Feedback, FeedbackSchema } from './schemas/feedback.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Feedback.name, schema: FeedbackSchema },
     ]),
+    AuthModule,
   ],
   providers: [FeedbackService],
   controllers: [FeedbackController],
