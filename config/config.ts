5e100cc7bff4ced7bf3e24307cbb54881a9f6071
tsnazar <tsnazar3@gmail.con>
2023-12-16 16:34:57 +0200

    feat: changes to config

--- config/config.ts
+++ config/config.ts
@@ -11,22 +11,14 @@ const getConfig: () => ConfigDTO = () => ({
   },
   postgres: {
     dialect: 'postgres',
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    url: process.env.POSTGRES_URL,
+    uri: process.env.POSTGRES_URL,
     define: {
       timestamps: false,
     },
     logging: false,
   },
   mongo: {
-    uri:
-      parseInt(process.env.MONGO_AUTH) == 0
-        ? `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
-        : `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
+    uri: process.env.MONGO_URL,
     options: {},
   },
 });
